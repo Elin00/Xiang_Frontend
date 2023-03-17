205fa4449af4a15ad78da12d6901933e80313f90
Elin <c2973457@gmail.com>
2023-03-17 16:29:06 +0800

    切換

--- src/views/Other/LoginRegisterWindow/LoginRegisterWindowModal.vue
+++ src/views/Other/LoginRegisterWindow/LoginRegisterWindowModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref, watch } from "vue";
 
 //Vue Material Kit 2 components
 import MaterialInput from "@/components/MaterialInput.vue";
@@ -14,6 +14,17 @@ onMounted(() => {
 });
 
 const Suppliersstore = useSuppliersDataStore();
+const isChecked = ref(false);
+const textOff = "會員";
+const textOn = "業者";
+
+// watch(isChecked, (value) => {
+//   if (value) {
+//     console.log("現在的資料屬於業者");
+//   } else {
+//     console.log("現在的資料屬於會員");
+//   }
+// });
 </script>
 
 
@@ -41,38 +52,96 @@ const Suppliersstore = useSuppliersDataStore();
                 </div>
                 <div class="card-body">
                   <form role="form" class="text-start">
-                    <div class="input-group input-group-outline my-3">
-                      <label class="form-label" data-bs-toggle="modal"
-                        >電子信箱</label
-                      ><!--v-if--><input
-                        id="email"
-                        type="email"
-                        class="form-control form-control-md"
-                        placeholder=""
-                        isrequired="true"
+                    <div class="checkbox-wrapper-10">
+                      <input
+                        class="tgl tgl-flip"
+                        id="cb5"
+                        type="checkbox"
+                        v-model="isChecked"
                       />
-                    </div>
-                    <div class="input-group input-group-outline mb-3">
-                      <label class="form-label">密碼</label
-                      ><!--v-if--><input
-                        id="password"
-                        type="password"
-                        class="form-control form-control-md"
-                        placeholder=""
-                        isrequired="true"
-                      />
-                    </div>
-                    <div class="form-check" style="padding-left: 0px">
-                      <a href="#" style="color: green">忘記密碼?</a>
+                      <label
+                        class="tgl-btn"
+                        :data-tg-off="textOff"
+                        :data-tg-on="textOn"
+                        for="cb5"
+                      ></label>
                     </div>
 
-                    <div class="text-center">
-                      <button
-                        class="btn bg-gradient-success btn-md w-100 false my-4 mb-2"
-                      >
-                        登入
-                      </button>
+                    <div>
+                      <div v-if="isChecked">
+                        <!-- 業者的登入畫面 -->
+                        會
+                        <div class="input-group input-group-outline my-3">
+                          <label class="form-label" data-bs-toggle="modal"
+                            >電子信箱</label
+                          ><!--v-if--><input
+                            id="email"
+                            type="email"
+                            class="form-control form-control-md"
+                            placeholder=""
+                            isrequired="true"
+                          />
+                        </div>
+                        <div class="input-group input-group-outline mb-3">
+                          <label class="form-label">密碼</label
+                          ><!--v-if--><input
+                            id="password"
+                            type="password"
+                            class="form-control form-control-md"
+                            placeholder=""
+                            isrequired="true"
+                          />
+                        </div>
+                        <div class="form-check" style="padding-left: 0px">
+                          <a href="#" style="color: green">忘記密碼?</a>
+                        </div>
+
+                        <div class="text-center">
+                          <button
+                            class="btn bg-gradient-success btn-md w-100 false my-4 mb-2"
+                          >
+                            登入
+                          </button>
+                        </div>
+                      </div>
+                      <div v-else>
+                        <!-- 會員的登入畫面 -->
+                        ya
+                        <div class="input-group input-group-outline my-3">
+                          <label class="form-label" data-bs-toggle="modal"
+                            >電子信箱</label
+                          ><!--v-if--><input
+                            id="email"
+                            type="email"
+                            class="form-control form-control-md"
+                            placeholder=""
+                            isrequired="true"
+                          />
+                        </div>
+                        <div class="input-group input-group-outline mb-3">
+                          <label class="form-label">密碼</label
+                          ><!--v-if--><input
+                            id="password"
+                            type="password"
+                            class="form-control form-control-md"
+                            placeholder=""
+                            isrequired="true"
+                          />
+                        </div>
+                        <div class="form-check" style="padding-left: 0px">
+                          <a href="#" style="color: green">忘記密碼?</a>
+                        </div>
+
+                        <div class="text-center">
+                          <button
+                            class="btn bg-gradient-success btn-md w-100 false my-4 mb-2"
+                          >
+                            登入
+                          </button>
+                        </div>
+                      </div>
                     </div>
+
                     <div class="order">
                       <span class="line"></span>
                       <span class="txt">或</span>
@@ -341,5 +410,111 @@ const Suppliersstore = useSuppliersDataStore();
 .order .txt {
   color: #686868;
   vertical-align: middle;
+}
+
+/* 切換按鈕 */
+.checkbox-wrapper-10 .tgl {
+  display: none;
+}
+.checkbox-wrapper-10 .tgl,
+.checkbox-wrapper-10 .tgl:after,
+.checkbox-wrapper-10 .tgl:before,
+.checkbox-wrapper-10 .tgl *,
+.checkbox-wrapper-10 .tgl *:after,
+.checkbox-wrapper-10 .tgl *:before,
+.checkbox-wrapper-10 .tgl + .tgl-btn {
+  box-sizing: border-box;
+}
+.checkbox-wrapper-10 .tgl::-moz-selection,
+.checkbox-wrapper-10 .tgl:after::-moz-selection,
+.checkbox-wrapper-10 .tgl:before::-moz-selection,
+.checkbox-wrapper-10 .tgl *::-moz-selection,
+.checkbox-wrapper-10 .tgl *:after::-moz-selection,
+.checkbox-wrapper-10 .tgl *:before::-moz-selection,
+.checkbox-wrapper-10 .tgl + .tgl-btn::-moz-selection,
+.checkbox-wrapper-10 .tgl::selection,
+.checkbox-wrapper-10 .tgl:after::selection,
+.checkbox-wrapper-10 .tgl:before::selection,
+.checkbox-wrapper-10 .tgl *::selection,
+.checkbox-wrapper-10 .tgl *:after::selection,
+.checkbox-wrapper-10 .tgl *:before::selection,
+.checkbox-wrapper-10 .tgl + .tgl-btn::selection {
+  background: none;
+}
+.checkbox-wrapper-10 .tgl + .tgl-btn {
+  outline: 0;
+  display: block;
+  width: 4em;
+  height: 2em;
+  position: relative;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.checkbox-wrapper-10 .tgl + .tgl-btn:after,
+.checkbox-wrapper-10 .tgl + .tgl-btn:before {
+  position: relative;
+  display: block;
+  content: "";
+  width: 50%;
+  height: 100%;
+}
+.checkbox-wrapper-10 .tgl + .tgl-btn:after {
+  left: 0;
+}
+.checkbox-wrapper-10 .tgl + .tgl-btn:before {
+  display: none;
+}
+.checkbox-wrapper-10 .tgl:checked + .tgl-btn:after {
+  left: 50%;
+}
+
+.checkbox-wrapper-10 .tgl-flip + .tgl-btn {
+  padding: 2px;
+  transition: all 0.2s ease;
+  font-family: sans-serif;
+  perspective: 100px;
+}
+.checkbox-wrapper-10 .tgl-flip + .tgl-btn:after,
+.checkbox-wrapper-10 .tgl-flip + .tgl-btn:before {
+  display: inline-block;
+  transition: all 0.4s ease;
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  line-height: 2em;
+  font-weight: bold;
+  color: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  border-radius: 4px;
+}
+.checkbox-wrapper-10 .tgl-flip + .tgl-btn:after {
+  content: attr(data-tg-on);
+  background: #02c66f;
+  transform: rotateY(-180deg);
+}
+.checkbox-wrapper-10 .tgl-flip + .tgl-btn:before {
+  background: #ff3a19;
+  content: attr(data-tg-off);
+}
+.checkbox-wrapper-10 .tgl-flip + .tgl-btn:active:before {
+  transform: rotateY(-20deg);
+}
+.checkbox-wrapper-10 .tgl-flip:checked + .tgl-btn:before {
+  transform: rotateY(180deg);
+}
+.checkbox-wrapper-10 .tgl-flip:checked + .tgl-btn:after {
+  transform: rotateY(0);
+  left: 0;
+  background: #7fc6a6;
+}
+.checkbox-wrapper-10 .tgl-flip:checked + .tgl-btn:active:after {
+  transform: rotateY(20deg);
 }
 </style>
