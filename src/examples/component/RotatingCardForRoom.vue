8fa10b9aa7bc0ba11e3ab9dfd761659e5b5339f2
DESKTOP-O90H2SN\林宥辰 <chen0927lin@gmail.com>
2023-03-20 00:01:44 +0800

    no message

--- src/examples/component/RotatingCardForRoom.vue
+++ src/examples/component/RotatingCardForRoom.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
-  <div class="rotating-card-container">
-    <div class="card card-rotate card-background card-background-mask-success shadow-success mt-md-0 mt-5">
+  <div class="rotating-card-container" >
+    <div class="card card-rotate card-background shadow-success mt-md-0 mt-5" style="height:400px">
       <slot />
     </div>
   </div>
