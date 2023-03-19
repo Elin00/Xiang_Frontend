29c3b327473e591f1250caaf425e3c6b0bdebf33
Elin <c2973457@gmail.com>
2023-03-20 00:15:40 +0800

    no message

--- src/views/Suppliers/AddRoomView.vue
+++ src/views/Suppliers/AddRoomView.vue
@@ -1,71 +1,103 @@
-<script lang="ts" setup>
-import { onMounted, ref, watch } from "vue";
-import { useSuppliersDataStore } from "../../stores/SuppliersData.js";
-import MaterialInput from "@/components/MaterialInput.vue";
-import MaterialButton from "@/components/MaterialButton.vue";
+<script setup>
+import { reactive, ref, onMounted } from "vue";
+import SupplierHeader from './Components/SupplierHeader.vue'
+import "../../assets/js/core/bootstrap.min.js";
+import PSite from './Components/PSite.vue'
 
+let AddSiteModal = null;
 
+const savePSite = () => {
 
-import { Delete, Download, Plus, ZoomIn } from '@element-plus/icons-vue'
-
-import type { UploadFile } from 'element-plus'
-
-const dialogImageUrl = ref('')
-const dialogVisible = ref(false)
-const disabled = ref(false)
-
-const handleRemove = (file: UploadFile) => {
-    console.log(file)
+}
+const addSiteModalOpen = () => {
+    AddSiteModal.show();
+    console.log(AddSiteModal.show);
+}
+const addSiteModalClose = () => {
+    AddSiteModal.hide();
+    console.log(AddSiteModal.hide);
 }
 
-const handlePictureCardPreview = (file: UploadFile) => {
-    dialogImageUrl.value = file.url!
-    dialogVisible.value = true
-}
-
-const handleDownload = (file: UploadFile) => {
-    console.log(file)
-}
-
-const Suppliersstore = useSuppliersDataStore();
-
+onMounted(() => {
+    AddSiteModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+})
 </script>
 
 <template>
-    <div style="margin-left: 10%;">
-        <h4>請填寫您的空間資訊</h4>
-        <h5>上傳您的空間照片</h5>
-        <el-upload action="#" list-type="picture-card" :auto-upload="false">
-            <el-icon>
-                <Plus />
-            </el-icon>
+    <SupplierHeader label-string="產品資訊" />
+    <div class="container">
+        <div class="row">
+            <ul class="list-unstyled d-flex border-bottom">
+                <li class="me-4">
+                    <h5>站點管理</h5>
+                </li>
+                <li class="me-4">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-success btn-sm" @click="addSiteModalOpen">新增</button>
+                </li>
+            </ul>
+        </div>
+        <PSite />
+    </div>
 
-            <template #file="{ file }">
-                <div>
-                    <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
-                    <span class="el-upload-list__item-actions">
-                        <span class="el-upload-list__item-preview" @click="handlePictureCardPreview(file)">
-                            <el-icon><zoom-in /></el-icon>
-                        </span>
-                        <span v-if="!disabled" class="el-upload-list__item-delete" @click="handleDownload(file)">
-                            <el-icon>
-                                <Download />
-                            </el-icon>
-                        </span>
-                        <span v-if="!disabled" class="el-upload-list__item-delete" @click="handleRemove(file)">
-                            <el-icon>
-                                <Delete />
-                            </el-icon>
-                        </span>
-                    </span>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                        <h3>新增站點</h3>
+                    </h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-            </template>
-        </el-upload>
+                <div class="modal-body">
+                    <h5>
+                        站點資訊
+                    </h5>
+                    <p>站點名稱</p>
+                    <div class="d-flex mb-3">
+                        <input type="text" class="form-control btn-light creditCardText" placeholder="請填入站點名字">
+                    </div>
+                    <p>圖片</p>
+                    <div class="d-flex mb-3">
+                        <input type="file" class="form-control btn-light creditCardText">
+                    </div>
+                    <p>開放時間</p>
+                    <div class="d-flex mb-3">
+                        <input type="text" class="form-control btn-light creditCardText"
+                            placeholder="08:00~12:00,13:00~17:00">
+                    </div>
+                    <p>地點</p>
+                    <div class="d-flex mb-3">
+                        <input type="text" class="form-control btn-light creditCardText" placeholder="請填入地址">
+                    </div>
+                    <p>描述</p>
+                    <div class="d-flex mb-3">
+                        <input type="text" class="form-control btn-light creditCardText" placeholder="簡述介紹這個站點">
+                    </div>
 
-        <el-dialog v-model="dialogVisible">
-            <img w-full :src="dialogImageUrl" alt="Preview Image" />
-        </el-dialog>
-
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" @click="addSiteModalClose">關閉</button>
+                    <button type="button" id="aa" class="btn btn-primary" @click="savePSite">儲存更新</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-<style></style>
+
+<style scoped>
+h5 {
+    color: gray;
+}
+
+.creditCardText {
+    text-align: center;
+    border: 1px solid gray;
+}
+
+input::placeholder {
+    text-align: center;
+}
+</style>
---
+++ src/views/Suppliers/Components/PSite.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { reactive, onMounted } from 'vue'
+
+const props = defineProps({
+    Name: {
+        type: String,
+        default: '崇明店',
+    },
+    OpenTime: {
+        type: String,
+        default: '08:00~12:00,13:00~17:00',
+    },
+    Address: {
+        type: String,
+        default: '701台南市東區崇明路589-7號',
+    },
+    Description: {
+        type: String,
+        default: '良好的空間，明亮的燈光',
+    },
+})
+
+const ot = reactive(['', '']);
+
+onMounted(() => {
+    let ar = props.OpenTime.split(',');
+    ar.forEach((i, idx) => ot[idx] = ar[idx]);
+});
+
+</script>
+
+<template>
+    <div class="row">
+        <div class="col-4">
+            <img class="w-100" src="../../../assets/img/SiteImg/bg9.jpg" alt="SiteImg">
+        </div>
+        <div class="col-2">
+            <h6>站點名稱</h6>
+            <p>{{ props.Name }}</p>
+            <h6>開放時間</h6>
+            <p class="mb-0">{{ ot[0] }}&ensp;{{ ot[1] }}</p>
+        </div>
+        <div class="col-4">
+            <h6>地址</h6>
+            <p>{{ props.Address }}</p>
+            <h6>描述</h6>
+            <p class="mb-0">{{ props.Description }}</p>
+        </div>
+        <div class="col-2">
+            <button class="btn btn-dark btn-sm" @click="">修改</button>
+            <br />
+            <button class="btn btn-danger btn-sm" @click="">刪除</button>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
