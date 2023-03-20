6937a1dafb7a7fdb76e5fa5dd3100cd2e601423f
unknown <111@gmail.com>
2023-03-20 20:19:57 +0800

    fix creditCardCard

--- src/views/Members/MemberDetail/MemberDetails.vue
+++ src/views/Members/MemberDetail/MemberDetails.vue
@@ -12,18 +12,18 @@ import MemberHeader from "../Components/MemberHeader.vue";
          <ul class="list-unstyled d-flex border-bottom">
             <li class="me-4">
                <RouterLink :to="{ name: 'member-memberdetail-information' }">
-                  <h5 class="linkstring" id="info">基本資料</h5>
+                  <h5 class="linkstring">基本資料</h5>
                </RouterLink>
             </li>
             <li class="me-4">
                <RouterLink :to="{ name: 'member-memberdetail-point' }">
-                  <h5 class="linkstring" id="point">累計積分</h5>
+                  <h5 class="linkstring">累計積分</h5>
                </RouterLink>
             </li>
             <!-- 直接導到ECPay -->
             <li class="me-4">
                <RouterLink :to="{ name: 'member-memberdetail-creditcard' }">
-                  <h5 class="linkstring" id="credit">信用卡管理</h5>
+                  <h5 class="linkstring">信用卡管理</h5>
                </RouterLink>
             </li>
          </ul>
